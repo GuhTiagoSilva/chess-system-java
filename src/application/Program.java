f68d6f19de31d8270f1ee8732b0279562be495e7
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2019-06-08 21:33:53 -0300

    Special Move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		
+
 		List<ChessPiece> captured = new ArrayList<>();
 
 		while (!chessMatch.getCheckMate()) {
@@ -37,9 +37,17 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
-				if(capturedPiece!=null) {
+
+				if (capturedPiece != null) {
 					captured.add(capturedPiece);
+				}
+
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion: (B/N/R/Q)");
+					String type = sc.nextLine();
+
+					chessMatch.replacePromotedPiece(type);
+
 				}
 
 			} catch (ChessException e) {
@@ -53,7 +61,7 @@ public class Program {
 
 		UI.clearScreen();
 		UI.printMatch(chessMatch, captured);
-		
+
 	}
 
 }
